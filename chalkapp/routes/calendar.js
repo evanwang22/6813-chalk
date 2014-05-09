09a48f77ea32d20c181de83ba4b2f6274bc220f7
Evan W <evanwang@mit.edu>
2014-05-09 09:18:37 +0000

    Calendar now gets the number of posts from that day

--- chalkapp/routes/calendar.js
+++ chalkapp/routes/calendar.js
@@ -24,4 +24,22 @@ router.get('/:day', function(req, res) {
   });
 });
 
+// API endpoint for getting the # of posts in a day
+router.get('/numposts/:day', function(req, res) {
+
+  var db = req.db
+  var collection = db.get('postcollection');
+  var connections = db.get('followers');
+  var users = [req.cookies.email]
+
+  connections.find({"follower": req.cookies.email}, function(err, docs) {
+    for (var i = 0; i < docs.length; i++) {
+      users.push(docs[i].user_email)
+    }
+    collection.find({"user_email": {$in : users}, "date": req.params.day}, function(e, docs) {
+      res.send({'posts':docs});
+    });
+  });
+});
+
 module.exports = router;
